fce0643a28357d340ab828ca64280445449e89f6
Long Le <olole005@edu.xamk.fi>
2019-06-13 22:27:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/longle/musicmachine/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.longle.musicmachine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Button mDownloadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDownloadButton = findViewById(R.id.downloadButton);
+
+        mDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        downloadSong();
+                    }
+                };
+                Thread thread = new Thread(runnable);
+                thread.setName("Download Thread");
+                thread.start();
+            }
+        });
+    }
+
+    private void downloadSong() {
+        long endTime = System.currentTimeMillis() +10*1000;
+        while(System.currentTimeMillis() < endTime){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, "Downloading Music");
+    }
+}
