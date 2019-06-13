e1ab7aafcf7a3078d3e8c96d431a19d7deea86c9
Long Le <olole005@edu.xamk.fi>
2019-06-13 22:48:12 +0700

    Adding A new Thread and the playlist songs

---
+++ app/src/main/java/com/example/longle/musicmachine/Playlist.java
@@ -0,0 +1,6 @@
+package com.example.longle.musicmachine;
+
+public class Playlist {
+    public static String[] songs = {"He’s the Greatest Dancer", "Cut the Cake",
+            "The Groove Line", "Stayin’ Alive", "Give Up the Funk"};
+}
